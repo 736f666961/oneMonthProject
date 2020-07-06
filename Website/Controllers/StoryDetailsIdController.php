1da70cd0e55d07439f2652a8d1ec1ddc997cc087
ABDERRAZZAK ELGHYATI <abderazakghiyati2@gmail.com>
2020-07-05 23:58:09 -0700

    Update website views

--- Website/Controllers/StoryDetailsIdController.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // This page is used to set session id and read that id from StorydetailsView page via ajax
-    session_start();
-    $id = $_POST['id'];
-    $_SESSION['id'] = $id;
-
-    // echo $id;
